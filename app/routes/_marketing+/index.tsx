dbccd90d9feaf71cff989b712ad64712d825400d
carlulsoe <34673973+carlulsoe@users.noreply.github.com>
2024-09-23 20:49:21 +0200

    removed unused imports

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,13 +1,5 @@
 import { type MetaFunction } from '@remix-run/node'
 import {
-	Tooltip,
-	TooltipContent,
-	TooltipProvider,
-	TooltipTrigger,
-} from '#app/components/ui/tooltip.tsx'
-import { cn } from '#app/utils/misc.tsx'
-import { logos } from './logos/logos.ts'
-import {
 	Card,
 	CardContent,
 	CardDescription,
@@ -16,24 +8,7 @@ import {
 	CardTitle,
 } from '#app/components/ui/card.tsx'
 
-export const meta: MetaFunction = () => [{ title: 'Epic Notes' }]
-
-// Tailwind Grid cell classes lookup
-const columnClasses: Record<(typeof logos)[number]['column'], string> = {
-	1: 'xl:col-start-1',
-	2: 'xl:col-start-2',
-	3: 'xl:col-start-3',
-	4: 'xl:col-start-4',
-	5: 'xl:col-start-5',
-}
-const rowClasses: Record<(typeof logos)[number]['row'], string> = {
-	1: 'xl:row-start-1',
-	2: 'xl:row-start-2',
-	3: 'xl:row-start-3',
-	4: 'xl:row-start-4',
-	5: 'xl:row-start-5',
-	6: 'xl:row-start-6',
-}
+export const meta: MetaFunction = () => [{ title: 'Stellar Ink' }]
 
 export default function Index() {
 	return (
