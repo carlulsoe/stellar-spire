3bcfb73a7e1284fb4ce1af15259e4e1f4c29b7be
carlulsoe <34673973+carlulsoe@users.noreply.github.com>
2024-09-29 17:29:58 +0200

    next chapter looks better

--- app/routes/users+/$username_+/stories+/$storyId_+/chapter+/$chapterId_.index.tsx
+++ app/routes/users+/$username_+/stories+/$storyId_+/chapter+/$chapterId_.index.tsx
@@ -3,6 +3,7 @@ import { json, type LoaderFunctionArgs } from "@remix-run/node"
 import { Link, useLoaderData } from "@remix-run/react"
 import { GeneralErrorBoundary } from "#app/components/error-boundary.js"
 import { prisma } from "#app/utils/db.server.js"
+import { Button } from "#app/components/ui/button.js"
 
 export async function loader({ params }: LoaderFunctionArgs) {
 	const chapter = await prisma.chapter.findUnique({
@@ -17,7 +18,7 @@ export async function loader({ params }: LoaderFunctionArgs) {
 	})
 
 	invariantResponse(chapter, 'Not found', { status: 404 })
-	
+
 	const nextChapter = await prisma.chapter.findFirst({
 		where: { storyId: params.storyId, number: chapter.number + 1 },
 		select: { id: true },
@@ -36,14 +37,15 @@ export default function ChapterRoute() {
 	return <div className="container pt-12">
 		<h1 className="text-h1">{data.chapter.title}</h1>
 		<div className="text-body-lg whitespace-break-spaces">{data.chapter.content}</div>
-		{data.nextChapter ? (
-			<Link
-				to={`/users/${data.username}/stories/${data.storyId}/chapter/${data.nextChapter.id}`}
-				className="mt-4 inline-block text-blue-600 hover:underline"
-			>
-				Next Chapter
-			</Link>
-		) : null}
+		{data.nextChapter ? (<div className="flex justify-end">
+			<Button>
+				<Link
+					to={`/users/${data.username}/stories/${data.storyId}/chapter/${data.nextChapter.id}`}
+				>
+					Next Chapter
+				</Link>
+			</Button>
+		</div>) : null}
 	</div>
 }
 
